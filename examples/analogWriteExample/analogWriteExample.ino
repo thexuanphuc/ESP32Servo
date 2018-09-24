64f3e3b28b33905d2f2b633ba36df929ff7fba5d
Kevin Harrington <mad.hephaestus@gmail.com>
2018-09-24 19:45:08 -0400

    Comments in the examples

--- examples/analogWriteExample/analogWriteExample.ino
+++ examples/analogWriteExample/analogWriteExample.ino
@@ -15,25 +15,19 @@
  */
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int lowestPin = 3;
-const int highestPin = 32;
+const int lowestPin = 2;
+const int highestPin = 33;
 #include <ESP32Servo.h>
 Servo myservo;
 
 void setup() {
 	Serial.begin(115200);
-	// set pins 2 through 13 as outputs:
-	for (int thisPin = lowestPin; thisPin <= highestPin; thisPin++) {
-		if (ESP32PWM::hasPwm(thisPin))// Only enable PWM capible pins as analog write
-			pinMode(thisPin, OUTPUT);
-	}
-
 }
 
 void loop() {
 	if (!myservo.attached()) {
 		myservo.setPeriodHertz(50);// standard 50 hz servo
-		myservo.attach(33, 1000, 2000);
+		myservo.attach(33, 1000, 2000);// Attach the servo after it has been detatched
 	}
 	myservo.write(0);
 	// iterate over the pins:
@@ -43,7 +37,10 @@ void loop() {
 						pwmFactory(thisPin) != NULL || // already allocated this pin in the factory
 						thisPin == 25 || // one of the 2 DAC outputs, no timer needed
 						thisPin == 26)) { // one of the 2 DAC outputs, no timer needed
-
+			if(pwmFactory(thisPin) == NULL){// check if its the first time for the pin or its a DAC
+				Serial.println("Writing to pin "+String(thisPin));
+				pinMode(thisPin, OUTPUT);
+			}
 			// fade the LED on thisPin from off to brightest:
 			for (int brightness = 0; brightness < 255; brightness++) {
 				analogWrite(thisPin, brightness);
