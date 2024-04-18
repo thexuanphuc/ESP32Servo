07cb9c0910514febab989c86b30b04ac1b13918f
Abishek S <abishek162019@gmail.com>
2024-04-18 20:21:55 +0530

    Implemented changes to work with ESP S3 and S2

--- src/ESP32Servo.cpp
+++ src/ESP32Servo.cpp
@@ -51,7 +51,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 #include <ESP32Servo.h>
-#include "Arduino.h"
+#if defined(ARDUINO)
+	#include "Arduino.h"
+#endif
 
 //
 Servo::Servo()
